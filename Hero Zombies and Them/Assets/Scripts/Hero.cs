998142f81d3dc94d98461f9ad0712fc2a29928bf
Bhalut <bhalut.bear@gmail.com>
2018-04-04 16:05:32 -0500

    Add Scripts

---
+++ Hero Zombies and Them/Assets/Scripts/Hero.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour 
+{
+    public float speed;
+
+	// Use this for initialization
+	void Start () 
+    {
+        speed = Random.Range(0.5f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Move();
+	}
+
+    void Move ()
+    {
+        
+    }
+}
---
+++ Hero Zombies and Them/Assets/Scripts/Zombie.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour 
+{
+    public enum State {Idle, Moving}
+    public enum Taste {arm, nose, ear, finger, leg}
+
+  
+    public State _state; 
+    public Taste _taste;
+
+	// Use this for initialization
+	void Start () 
+    {
+        _state = State.Idle;
+        _taste = Taste.arm;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        
+	}
+
+    void ZombieBehavior(int i, int j)
+    {
+        //Moving
+        switch(i)
+        {
+            case 0:
+                _state = State.Idle;
+                break;
+            case 1:
+                _state = State.Moving;
+                break;
+            default:
+                _state = State.Idle;
+                break;
+        }
+
+        //Taste
+        switch(j)
+        {
+            case 0:
+                _taste = Taste.arm;
+                break;
+            case 1:
+                _taste = Taste.ear;
+                break;
+            case 2:
+                _taste = Taste.finger;
+                break;
+            case 3:
+                _taste = Taste.leg;
+                break;
+            case 4:
+                _taste = Taste.nose;
+                break;
+            default:
+                _taste = Taste.arm;
+                break;
+        }
+    }
+    void Message()
+    {
+        print("Waaaaarrrr quiero comer ");
+    }
+}
